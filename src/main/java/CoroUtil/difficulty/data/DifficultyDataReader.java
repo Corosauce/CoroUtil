b26d5760a1c4e293dd28210265e62f1e65feab15
Corosauce <cordonfreeman@gmail.com>
2017-02-20 02:39:20 -0500

    Prototyping new condition - action design for json backed cmod system

--- src/main/java/CoroUtil/difficulty/data/DifficultyDataReader.java
+++ src/main/java/CoroUtil/difficulty/data/DifficultyDataReader.java
@@ -91,7 +91,7 @@ public class DifficultyDataReader {
             if (lastPath.endsWith(lootTablesFolder)) {
                 String fileContents = Files.toString(file, Charsets.UTF_8);
                 String fileName = file.getName().replace(".json", "");
-                ResourceLocation resName = new ResourceLocation(CoroUtil.modID + ":loot_tables" + fileName);
+                ResourceLocation resName = new ResourceLocation(CoroUtil.modID + ":loot_tables." + fileName);
                 LootTable lootTable = net.minecraftforge.common.ForgeHooks.loadLootTable(LootTableManager.GSON_INSTANCE, resName, fileContents, true);
                 data.lookupLootTables.put(fileName, lootTable);
             } else {
