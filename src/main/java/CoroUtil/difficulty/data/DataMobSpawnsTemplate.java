e0324ae1710e83f2a3f6f5ffa9ee0927de04caa8
Corosauce <cordonfreeman@gmail.com>
2017-03-07 22:37:31 -0500

    Initial working test code for selecting an invasion profile based on the conditions and condition templates used

--- src/main/java/CoroUtil/difficulty/data/DataMobSpawnsTemplate.java
+++ src/main/java/CoroUtil/difficulty/data/DataMobSpawnsTemplate.java
@@ -1,6 +1,9 @@
 package CoroUtil.difficulty.data;
 
+import CoroUtil.difficulty.data.conditions.ConditionTemplate;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -12,4 +15,56 @@ public class DataMobSpawnsTemplate {
     public List<DataCondition> conditions = new ArrayList<>();
     public List<DataActionMobSpawns> spawns = new ArrayList<>();
 
+    public List<DataCondition> getConditions() {
+        return conditions;
+    }
+
+    /**
+     * Get conditions including ones nested in templates
+     * - aiming to use overridable conditions
+     * - top level condition overrides one in a template if are of the same type
+     * - there may be scenarios where i want to layer and use both conditions?
+     * - what about 2 templates referenced, each with a random?
+     * @return
+     */
+    public List<DataCondition> getConditionsFlattened() {
+        //List<DataCondition> listTop = new ArrayList<>();
+        HashMap<String, DataCondition> lookup = new HashMap<>();
+        //conditions.contains()
+
+        for (DataCondition condition : conditions) {
+            if (condition instanceof ConditionTemplate) {
+
+            } else {
+                if (!lookup.containsKey(condition.condition)) {
+                    lookup.put(condition.condition, condition);
+                } else {
+                    CoroUtil.forge.CoroUtil.dbg("duplicate key for condition at top level " + condition.condition);
+                }
+            }
+
+        }
+
+        for (DataCondition condition : conditions) {
+            if (condition instanceof ConditionTemplate) {
+                DataConditionTemplate template = DifficultyDataReader.getData().lookupConditionTemplates.get(((ConditionTemplate) condition).template);
+                if (template != null) {
+                    for (DataCondition condition2 : template.conditions) {
+                        //prevent nested templating for now
+                        if (!(condition2 instanceof ConditionTemplate)) {
+                            if (!lookup.containsKey(condition2.condition)) {
+                                lookup.put(condition2.condition, condition2);
+                            } else {
+                                CoroUtil.forge.CoroUtil.dbg("duplicate key for condition from template " + condition2.condition);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        List<DataCondition> list = new ArrayList<>();
+        list.addAll(lookup.values());
+        return list;
+    }
 }
