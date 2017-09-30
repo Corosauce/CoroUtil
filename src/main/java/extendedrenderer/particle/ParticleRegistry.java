4069d140ff8bf97d2b0ff2f09e15e188cd23d6ae
Corosauce <cordonfreeman@gmail.com>
2017-09-29 23:10:35 -0400

    Added rest of fish, tweaks

--- src/main/java/extendedrenderer/particle/ParticleRegistry.java
+++ src/main/java/extendedrenderer/particle/ParticleRegistry.java
@@ -5,6 +5,9 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import extendedrenderer.ExtendedRenderer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParticleRegistry {
 
 	public static TextureAtlasSprite squareGrey;
@@ -37,7 +40,8 @@ public class ParticleRegistry {
 	public static TextureAtlasSprite debris_1;
 	public static TextureAtlasSprite debris_2;
 	public static TextureAtlasSprite debris_3;
-	public static TextureAtlasSprite fish_1;
+	public static List<TextureAtlasSprite> listFish = new ArrayList<>();
+	//public static TextureAtlasSprite fish_1;
 	public static TextureAtlasSprite test_texture;
 	
 	public static void init(TextureStitchEvent.Pre event) {
@@ -76,7 +80,9 @@ public class ParticleRegistry {
 		debris_1 = event.getMap().registerSprite(new ResourceLocation(ExtendedRenderer.modid + ":particles/debris_1"));
 		debris_2 = event.getMap().registerSprite(new ResourceLocation(ExtendedRenderer.modid + ":particles/debris_2"));
 		debris_3 = event.getMap().registerSprite(new ResourceLocation(ExtendedRenderer.modid + ":particles/debris_3"));
-		fish_1 = event.getMap().registerSprite(new ResourceLocation(ExtendedRenderer.modid + ":particles/fish_1"));
+		for (int i = 1; i <= 9; i++) {
+			listFish.add(event.getMap().registerSprite(new ResourceLocation(ExtendedRenderer.modid + ":particles/fish_" + i)));
+		}
 
 
 		//ParticleMeshBufferManager.setupMeshForParticle(cloud256_6);
