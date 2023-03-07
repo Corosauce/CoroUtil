2fc302b9f9b70a1b567a9db269673237928ba5d0
Corosauce <cordonfreeman@gmail.com>
2023-03-07 00:29:23 +0000

    Serene Seasons compat

--- src/main/java/com/corosus/coroutil/util/CoroUtilCompatibility.java
+++ src/main/java/com/corosus/coroutil/util/CoroUtilCompatibility.java
@@ -1,11 +1,21 @@
 package com.corosus.coroutil.util;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.biome.Biome;
 
+import java.lang.reflect.Method;
+
 public class CoroUtilCompatibility {
+
+    private static boolean sereneSeasonsInstalled = false;
+    private static boolean checksereneSeasons = true;
+
+    private static Class class_SereneSeasons_ASMHelper = null;
+    private static Method method_sereneSeasons_getFloatTemperature = null;
 
     /**
      * Used to contain compat with other mods, still used incase i add that back in
@@ -25,8 +35,54 @@ public class CoroUtilCompatibility {
     }
 
     public static float getAdjustedTemperature(Level world, Biome biome, BlockPos pos) {
-        //TODO: serene seasons compat again
-        return biome.getTemperature(pos);
+        if (isSereneSeasonsInstalled()) {
+            try {
+                if (method_sereneSeasons_getFloatTemperature == null) {
+                    method_sereneSeasons_getFloatTemperature = class_SereneSeasons_ASMHelper.getDeclaredMethod("getBiomeTemperature", Level.class, Holder.class, BlockPos.class);
+                }
+                Holder<Biome> biomeHolder = world.m_204166_(pos);
+                return (float) method_sereneSeasons_getFloatTemperature.invoke(null, world, biomeHolder, pos);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                //prevent error spam
+                sereneSeasonsInstalled = false;
+                return biome.getTemperature(pos);
+            }
+        } else {
+            return biome.getTemperature(pos);
+        }
+    }
+
+    /**
+     * Check if Serene Seasons is installed
+     *
+     * @return
+     */
+    public static boolean isSereneSeasonsInstalled() {
+        if (checksereneSeasons) {
+            try {
+                checksereneSeasons = false;
+                class_SereneSeasons_ASMHelper = Class.forName("sereneseasons.season.SeasonHooks");
+                if (class_SereneSeasons_ASMHelper != null) {
+                    sereneSeasonsInstalled = true;
+                }
+            } catch (Exception ex) {
+                //not installed
+                //ex.printStackTrace();
+            }
+
+            CULog.log("CoroUtil detected Serene Seasons " + (sereneSeasonsInstalled ? "Installed" : "Not Installed") + " for use");
+        }
+
+        return sereneSeasonsInstalled;
+    }
+
+    public static boolean coldEnoughToSnow(Biome biome, BlockPos pos, Level levelReader) {
+        return !warmEnoughToRain(biome, pos, levelReader);
+    }
+
+    public static boolean warmEnoughToRain(Biome biome, BlockPos pos, Level levelReader) {
+        return getAdjustedTemperature(levelReader, biome, pos) >= 0.15F;
     }
 
 }
