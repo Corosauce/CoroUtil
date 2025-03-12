e112436d9a0e802a4e818be35e1bca5f58dcfb1e
Corosauce <cordonfreeman@gmail.com>
2025-03-12 10:11:43 +0000

    cleanup

--- build-fabric.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del build.gradle
-copy build_fabric.gradle build.gradle
-start gradlew build
--- build-forge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del build.gradle
-copy build_forge.gradle build.gradle
-start gradlew build
